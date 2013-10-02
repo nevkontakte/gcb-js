f3695658043c0ce940beca62e9d5c760064702ec
Aleks Nevkontakte <aleks@aradmin.org.ru>
2013-10-02 15:15:52 +0700

    UPD: Implemented GCB v3.0

--- gcache.js
+++ gcache.js
@@ -1,153 +1,20 @@
-var gcb = new function () {
-	var self = this;
+/*!
+ * $script.js Async loader & dependency manager
+ * https://github.com/ded/script.js
+ * (c) Dustin Diaz 2013
+ * License: MIT
+ */
+// noinspection All
+(function(e,t,n){window['$script']=n()})("$script",this,function(){function v(e,t){for(var n=0,r=e.length;n<r;++n)if(!t(e[n]))return f;return 1}function m(e,t){v(e,function(e){return!t(e)})}function g(e,t,a){function d(e){return e.call?e():r[e]}function b(){if(!--p){r[h]=1,c&&c();for(var e in s)v(e.split("|"),d)&&!m(s[e],d)&&(s[e]=[])}}e=e[l]?e:[e];var f=t&&t.call,c=f?t:a,h=f?e.join(""):t,p=e.length;return setTimeout(function(){m(e,function(e){if(e===null)return b();if(u[e])return h&&(i[h]=1),u[e]==2&&b();u[e]=1,h&&(i[h]=1),y(!n.test(e)&&o?o+e+".js":e,b)})},0),g}function y(n,r){var i=e.createElement("script"),s=f;i.onload=i.onerror=i[d]=function(){if(i[h]&&!/^c|loade/.test(i[h])||s)return;i.onload=i[d]=null,s=1,u[n]=2,r()},i.async=1,i.src=n,t.insertBefore(i,t.firstChild)}var e=document,t=e.getElementsByTagName("head")[0],n=/^https?:\/\//,r={},i={},s={},o,u={},a="string",f=!1,l="push",c="DOMContentLoaded",h="readyState",p="addEventListener",d="onreadystatechange";return!e[h]&&e[p]&&(e[p](c,function b(){e.removeEventListener(c,b,f),e[h]="complete"},f),e[h]="loading"),g.get=y,g.order=function(e,t,n){(function r(i){i=e.shift(),e.length?g(i,r):g(i,t,n)})()},g.path=function(e){o=e},g.ready=function(e,t,n){e=e[l]?e:[e];var i=[];return!m(e,function(e){r[e]||i[l](e)})&&v(e,function(e){return r[e]})?t():!function(e){s[e]=s[e]||[],s[e][l](t),n&&n(i)}(e.join("|")),g},g.done=function(e){g([null],e)},g})
 
-	this.loadJQuery = function (document, callback) {
-		// Load jQuery from Google CDN
-		var jqLoad = document.createElement("script");
-		jqLoad.setAttribute("type", "text/javascript");
-		jqLoad.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js");
-		if(document.head != null) {
-			document.head.appendChild(jqLoad);
-		} else if(document.body != null) {
-			document.body.appendChild(jqLoad);
-		} else {
-			document.appendChild(jqLoad);
-		}
-
-		// Wait until jQuery is loaded. Check it every 200 ms.
-		var timer = null;
-		var wait = function () {
-			var silence = true;
-			try {
-				if (jQuery != undefined) {
-					silence = false;
-					callback.apply(this, []);
-					return;
-				}
-			}
-			catch (e) {
-				if (!silence) {
-					throw e;
-				}
-			}
-			timer = setTimeout(wait, 200);
-		};
-
-		timer = setTimeout(wait, 200);
-	};
-
-	this.go = function (url) {
-		var query = "http://webcache.googleusercontent.com/search?q=" + encodeURIComponent("cache:" + url);
-		if(url == "") {
-			query = "about:blank";
-		}
-		$(".overlay.load").fadeIn('fast');
-		$(".viewport").attr('src', query);
-		$(".url input").val(url);
-	};
-
-	this.goHash = function() {
-		var hash = document.location.hash;
-		if(hash.indexOf("#go=") == 0) {
-			var url = hash.replace(/#go=/i, '');
-			self.go(url);
-		}
-	}
-
-	this.init = function () {
-		$("div").hide();
-		var toolbar = $("<div class='toolbar'></div>");
-		
-		var logo = $("<div class='logo'><p class='darkblue'><a href='http://cache.nevkontakte.com/'><span class='google'>" +
-				"<span class='blue'>G</span>" +
-				"<span class='red'>o</span>" +
-				"<span class='yellow'>o</span>" +
-				"<span class='blue'>g</span>" +
-				"<span class='green'>l</span>" +
-				"<span class='red'>e</span>" +
-				"</span>&#153; cache browser" +
-				"</a></p></div>");
-		logo.appendTo(toolbar);
-
-		var urlBar = $("<div class='url'><form action='#'><input type='text' id='url'></form><div class='go'>&rarr;</div></div>");
-		var go = $(".go", urlBar);
-		var url = $("input", urlBar);
-		urlBar.appendTo(toolbar);
-
-		go.click(function () {
-			self.go(url.val());
-		});
-		$("form", urlBar).submit(function () {
-			self.go(url.val());
-			return false;
-		});
-
-		toolbar.appendTo(document.body);
-
-		var view = $("<div class='view'><iframe class='viewport' src='about:blank'></iframe></div> ");
-		view.css('margin-top', toolbar.height() + 2 + "px");
-		var viewport = $("iframe", view);
-		var loadOverlay = $("<div class='overlay load'><img src='images/ajax-loader.gif' alt='Loading...'></div>");
-		loadOverlay.hide();
-		loadOverlay.appendTo(view);
-
-		viewport.load(function(){
-			var frame = this;
-			loadOverlay.fadeOut('fast');
-
-			try {
-				$("a", frame.contentDocument).each(function(){
-					$(this).click(function(){
-						self.go(this.href);
-						return false;
-					});
-				});
-			}
-			catch (e) {
-
-			}
-		});
-
-		var resizer = function () {
-			// Resize iframe
-			view.css('margin-top', toolbar.height() + 1 + "px");
-			view.height($(window).height() - toolbar.outerHeight() - 1);
-
-			// Resize url bar
-			var width = toolbar.width();
-			width -= logo.outerWidth(true);
-			width -= urlBar.outerWidth(true) - urlBar.width()+20;
-			urlBar.width(width);
-
-			// Resize url input
-			width = urlBar.width();
-			width -= go.outerWidth(true);
-			width -= url.outerWidth(true) - url.width()+20;
-			url.width(width);
-		};
-
-		resizer();
-		$(window).resize(resizer);
-		$(document).load(function(){
-			resizer();
-		});
-
-		view.appendTo(document.body);
-
-		url.focus();
-
-		self.goHash();
-	};
-
-	// Preload images
-	var preload = function (){
-		var loading = new Image();
-		loading.src = "images/ajax-loader.gif";
-		var bg = new Image();
-		bg.src = "images/overlay.png";
-	}();
-
-	this.loadJQuery(document, function(){
-		self.init();
-	});
-};
+// Declare modules
+$script('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js', function() {
+    "use strict";
+    $script('http://canjs.com/release//1.1.6/can.jquery.js', function() {
+        $script(['js/bootstrap.js', 'js/gcb.js'], function() {
+            $(function(){
+                new Gcb.Navigator('body');
+            })
+        });
+    });
+});
--- js/gcb.js
+++ js/gcb.js
@@ -1,7 +1,5 @@
-var Gcb = (function() {
+var Gcb = (function(publish) {
     "use strict";
-
-    var publish = {};
 
     publish.Form = can.Control({
         /**
@@ -12,6 +10,9 @@ var Gcb = (function() {
         'submit': function(el, event) {
             event.preventDefault();
             can.route.attr('url', this.element.find('input[name=url]').val())
+        },
+        'go/:url route': function(data) {
+            this.element.find('input[name=url]').val(data['url'])
         }
     });
 
@@ -31,7 +32,7 @@ var Gcb = (function() {
     });
 
     publish.Bookmarklet = can.Control({
-        init: function(el) {
+        init: function() {
             var bookmarklet = function() {
                 var loader = document.createElement("script");
                 loader.setAttribute("type", "text/javascript");
@@ -49,5 +50,120 @@ var Gcb = (function() {
         }
     });
 
+    publish.Navbar = can.Control({
+        init: function(el) {
+            el.attr('class', 'navbar navbar-static-top navbar-inverse');
+            $('<div class="navbar-inner">\
+                <div class="container">\
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                </a>\
+                \
+                <a class="brand" href="http://cache.nevkontakte.com/">Google Cache Browser</a>\
+                <form class="gcb-form navbar-search pull-left">\
+                    <div class="input-append">\
+                    <input type="text" class="span5" placeholder="Enter web page URL" name="url">\
+                    <button type="submit" class="btn">\
+                    <i class="icon-long-arrow-right"></i>\
+                    </button>\
+                    </div>\
+                </form>\
+            <div class="nav-collapse collapse">\
+                <ul class="nav nav-pills pull-right">\
+                <li><a href="#!about"><i class="icon-info-sign"></i> About</a></li>\
+                <li><a href="#!contact"><i class="icon-envelope"></i> Contact</a></li>\
+                <li><a href="https://github.com/nevkontakte/gcb-js"><i class="icon-github"></i> GitHub</a></li>\
+                </ul>\
+                </div>\
+            </div>').appendTo(el);
+            new publish.Form($('.gcb-form', el));
+        }
+    });
+
+    publish.Viewport = can.Control({
+        init: function(el, options) {
+            /**
+             * @type {jQuery}
+             */
+            this.navbar = options.navbar;
+
+            this.view = $('<iframe frameborder="0">');
+            this.view.css('background', '#FFF');
+            this.view.css('width', '100%');
+
+            this.card = $('<div class="card">');
+            this.element.append(this.card.append(this.view));
+            this.element.addClass('container gcb-viewport');
+
+            this.view.load(function(){
+                var frame = this;
+                $("a", frame.contentDocument).each(function(){
+                    $(this).click(function(e){
+                        e.preventDefault();
+                        can.route.attr('url', this.href);
+                        console.log(this.href);
+                        return false;
+                    });
+                });
+                alert('Done');
+
+            });
+        },
+
+        'resize': function() {
+            this.view.height(
+                $(window).height()
+                - this.element.offset().top
+                - (this.card.outerHeight(true) - this.card.height())
+
+            );
+        },
+
+        '{window} resize': function() {
+            this.resize();
+        },
+
+        'go/:url route': function(data) {
+            var query = "http://webcache.googleusercontent.com/search?q=" + encodeURIComponent("cache:" + data['url']);
+            if(data['url'] == "") {
+                query = "about:blank";
+            }
+            this.view.get(0).contentDocument.location.replace(query);
+            
+            var cd = this.view.get(0).contentDocument;
+            $(cd).ready(function(){
+                alert(cd.location.href);
+            });
+        }
+    });
+
+    publish.Navigator = can.Control({
+        init: function(el) {
+            $('#greetings').remove();
+            $('body *').hide();
+
+            var meta = [
+                {'name':'viewport', 'content':'width=device-width, initial-scale=1.0'}
+            ];
+
+            var head = $('head');
+            for(var i = 0; i < meta.length; i++) {
+                var tag = $('<meta>');
+                tag.attr(meta[i]);
+                head.append(tag);
+            }
+
+            var navbar = new publish.Navbar($('<div>'));
+            el.append(navbar.element);
+            var viewport = new publish.Viewport($('<div>'), {navbar: navbar});
+            el.append(viewport.element);
+            viewport.resize();
+
+            can.route.ready(true)
+        }
+    });
+
     return publish;
-}());
+}(Gcb || {}));
